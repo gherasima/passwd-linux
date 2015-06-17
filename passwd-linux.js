dc9700c058fce7787981ca9c215d9f9dbecab610
Adrian Gherasim <gherasim.a@me.com>
2015-06-17 18:09:43 +0300

    add more function

--- passwd-linux.js
+++ passwd-linux.js
@@ -1,3 +1,7 @@
+// Global variables
+// var shadowLocation = '/etc/shadow';
+var shadowLocation = '/tmp/shadowsha512';
+
 function checkPassSHA512(username, password, callback) {
     "use strict";
 
@@ -6,7 +10,6 @@ function checkPassSHA512(username, password, callback) {
     var sha512crypt = require('sha512crypt-node');
 
     // Static Parameters
-    var shadowLocation = '/etc/shadow';
     var userNameInput = username;
     var passwordCheck = password;
 
@@ -62,28 +65,98 @@ function changePass(username, password, newPassword, callback) {
 
     // First check user and password
     checkPassSHA512(username, password, function (error, response) {
-            if (error) {
-                console.log(error);
-            }
+        if (error) {
+            console.log(error);
+        }
 
-            // if password correct, change user password
-            if (response === 'passwordCorrect') {
-                exec('echo $pass | passwd --stdin $user', {
-                    env: {
-                        user: username,
-                        pass: newPassword
-                    }
-                });             //
-                callback(null, 'passChangeOK');
-            } else if (response === 'passwordIncorrect') {
-                callback(null, 'passChangeERROR');
-            } else if (response === 'unknownUser') {
-                callback(null, 'passChangeERROR');
-            } else {
-                callback(null, 'passChangeERROR');
+        // if password correct, change user password
+        if (response === 'passwordCorrect') {
+            exec('echo $pass | passwd --stdin $user', {
+                env: {
+                    user: username,
+                    pass: newPassword
+                }
+            });             //
+            callback(null, 'passChangeOK');
+        } else if (response === 'passwordIncorrect') {
+            callback(null, 'passChangeERROR');
+        } else if (response === 'unknownUser') {
+            callback(null, 'passChangeERROR');
+        } else {
+            callback(null, 'passChangeERROR');
+        }
+    });
+}
+
+function changePassNV(username, newPassword, callback) {
+    "use strict";
+
+    // Require
+    var exec = require("child_process").exec;
+
+    // First check user and password
+    checkPassSHA512(username, password, function (error, response) {
+        if (error) {
+            console.log(error);
+        }
+
+        // if password correct, change user password
+        if (response === 'passwordCorrect') {
+            exec('echo $pass | passwd --stdin $user', {
+                env: {
+                    user: username,
+                    pass: newPassword
+                }
+            });             //
+            callback(null, 'passChangeOK');
+        } else if (response === 'passwordIncorrect') {
+            callback(null, 'passChangeERROR');
+        } else if (response === 'unknownUser') {
+            callback(null, 'passChangeERROR');
+        } else {
+            callback(null, 'passChangeERROR');
+        }
+    });
+}
+
+function checkUser(username, callback) {
+    "use strict";
+
+    // Require
+    var fs = require('fs');
+
+    // Static Parameters
+    var userNameInput = username;
+
+    // Open shadow file
+    fs.readFile(shadowLocation, function (error, file) {
+        if (error) {
+            return callback(error); // file does not exit
+        }
+        // file is a buffer, convert to string and then to array
+        var shadowFile = file.toString().split('\n');
+
+        // Check if user exist on the shadow file
+        //  If user exist, set variable 'passwordHashFromFile' to contain full password
+        var passwordHashFromFile;
+        shadowFile.forEach(function (line) {
+            var shadowLineArray = line.split(":");
+            var userNameFromFile = shadowLineArray[0];
+            if (userNameFromFile === userNameInput) {
+                passwordHashFromFile = shadowLineArray[1];
             }
         });
+        if (passwordHashFromFile) {
+            callback(null, 'userExist');
+        } else {
+            callback(null, 'unknownUser');
+        }
+
+    });
 }
+
 
 module.exports.checkPassSHA512 = checkPassSHA512;
 module.exports.changePass = changePass;
+module.exports.checkUser = checkUser;
+module.exports.changePassNV = changePassNV;
